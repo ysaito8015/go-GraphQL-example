80e91eeb9d93e49de0839d0d4a60f236d9069caa
Yusuke_Saito <info@ysaito.dev>
2024-10-01 17:29:13 +0900

    done ch04

--- src/internal/server/main.go
+++ src/internal/server/main.go
@@ -1,18 +1,25 @@
 package main
 
 import (
+	"database/sql"
+	"fmt"
 	"log"
 	"net/http"
 	"os"
 
 	"github.com/99designs/gqlgen/graphql/handler"
 	"github.com/99designs/gqlgen/graphql/playground"
+	_ "github.com/mattn/go-sqlite3"
 
-	graph "mygraphql/graph"
-	internal "mygraphql/internal"
+	"mygraphql/graph"
+	"mygraphql/graph/services"
+	"mygraphql/internal"
 )
 
-const defaultPort = "8080"
+const (
+	defaultPort = "8080"
+	dbFile      = "./mygraphql.db"
+)
 
 func main() {
 	port := os.Getenv("PORT")
@@ -20,7 +27,21 @@ func main() {
 		port = defaultPort
 	}
 
-	srv := handler.NewDefaultServer(internal.NewExecutableSchema(internal.Config{Resolvers: &graph.Resolver{}}))
+	db, err := sql.Open("sqlite3", fmt.Sprintf("%s?_foreign_keys=on", dbFile))
+	if err != nil {
+		log.Fatalf("failed to open database: %v", err)
+	}
+	defer db.Close()
+
+	service := services.New(db)
+
+	srv := handler.NewDefaultServer(
+		internal.NewExecutableSchema(internal.Config{
+			Resolvers: &graph.Resolver{
+				Srv: service,
+			},
+		}),
+	)
 
 	http.Handle("/", playground.Handler("GraphQL playground", "/query"))
 	http.Handle("/query", srv)
